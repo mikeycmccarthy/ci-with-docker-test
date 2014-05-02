850a9aee97bc5cb53dc42de4f2898b54eb6e24ea
Michael McCarthy <michael.mccarthy@GL04632M.local>
2014-05-02 13:52:06 +0100

    Test project to trial docker Maven plugin

---
+++ src/main/java/com/mikeycmccarthy/Main.java
@@ -0,0 +1,12 @@
+package com.mikeycmccarthy;
+
+/**
+ * Created by michael.mccarthy on 02/05/2014.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Main.main");
+    }
+
+}
